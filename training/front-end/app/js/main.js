1b38f39908952cd25184c09714d08754ef19f63a
wildergonzo <wilder_gonzo@yahoo.com>
2015-06-19 15:18:55 -0400

    converted functions to class Calculator methods

--- training/front-end/app/js/main.js
+++ training/front-end/app/js/main.js
@@ -1,65 +1,55 @@
-/**
- * Created by Wilder Gonzales on 6/6/2015.
- */
-
-var calculateAll = function () {
-    //var args = Array.prototype.slice.call(arguments); //alternative for sum1
-    console.log('min: ' + min(arguments, 0));
-    console.log('max: ' + max(arguments, 0));
-    console.log('average: ' + avg(arguments, 0, arguments.length));
-    console.log('sum: ' + sum(arguments, 0));
-};
-
-var min = function (numbers, pos) {
-    if (pos == numbers.length - 1) {
-        return numbers[pos];
-    }
-
-    var val = min(numbers, pos + 1);
-
-    if (numbers[pos] < val) {
-        return numbers[pos];
-    } else {
-        return val;
-    }
-};
-
-var max = function (numbers, pos) {
-    if (pos == numbers.length - 1) {
-        return numbers[pos];
-    }
-
-    var val = max(numbers, pos + 1);
-
-    if (numbers[pos] > val) {
-        return numbers[pos];
-    } else {
-        return val;
-    }
-};
-
-var avg = function (numbers, pos, n) {
-    if (pos == numbers.length - 1) {
-        return numbers[pos];
-    }
-    if (pos == 0) {
-        return ((numbers[pos] + avg(numbers, pos + 1, n)) / n);
-    } else {
-        return (numbers[pos] + avg(numbers, pos + 1, n));
-    }
-};
-
-var sum = function (numbers, pos) {
-    if (pos == numbers.length - 1) {
-        return numbers[pos];
-    }
-    return numbers[pos] + sum(numbers, pos + 1);
-}
-
-var sum1 = function (array) {
-    if (array.length === 0) {
-        return 0;
-    } else return array.shift() + sum(array);
-};
-
-calculateAll(1,2,3,4,5,6,7,8);
+(function() {
+    "use strict";
+    var Calculator = function () {   
+        
+        this.min = function (numbers, pos) {
+            if (pos === numbers.length - 1) {
+                return numbers[pos];
+            }           
+            var val = this.min(numbers, pos + 1);         
+            if (numbers[pos] < val) {
+                return numbers[pos];
+            } else {
+                return val;
+            }
+        };
+        
+        this.max = function (numbers, pos) {
+            if (pos === numbers.length - 1) {
+                return numbers[pos];
+            }
+            var val = this.max(numbers, pos + 1);
+            if (numbers[pos] > val) {
+                return numbers[pos];
+            } else {
+                return val;
+            }
+        };
+        
+        this.avg = function (numbers, pos, n) {
+            if (pos === numbers.length - 1) {
+                return numbers[pos];
+            }
+            if (pos === 0) {
+                return ((numbers[pos] + this.avg(numbers, pos + 1, n)) / n);
+            } else {
+                return (numbers[pos] + this.avg(numbers, pos + 1, n));
+            }
+        };
+        
+        this.sum = function (numbers, pos) {
+            if (pos === numbers.length - 1)
+                return numbers[pos];              
+            return numbers[pos] + this.sum(numbers, pos + 1);
+        };
+        
+        this.calculateAll = function () {
+            console.log('min: ' + this.min(arguments, 0));
+            console.log('max: ' + this.max(arguments, 0));
+            console.log('average: ' + this.avg(arguments, 0, arguments.length));
+            console.log('sum: ' + this.sum(arguments, 0));
+        };          
+    };   
+    var calc = new Calculator();
+    calc.calculateAll(1,2,3,4,5,6,7,8);        
+}());
