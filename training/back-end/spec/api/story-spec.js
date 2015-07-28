1cf6accc4226ec50ce6c5a01af1ec17532e091a7
wildergonzo <wilder_gonzo@yahoo.com>
2015-07-27 23:16:39 -0400

    frisby: added story tests

---
+++ training/back-end/spec/api/story-spec.js
@@ -0,0 +1,37 @@
+var frisby = require('frisby');
+var storyId = 652018;
+var expectedComments = [1310, 1309];
+
+frisby.globalSetup({
+	request: {
+		headers:{
+			'Authorization': 'Basic d2lsZGVyZ29uem8udGVzdEBnbWFpbC5jb206Q29udHJvbDEyMw=='
+		}
+	}
+});
+
+var storiesMatch = true;
+frisby.create('agilefant should return the story data given an id')
+	.get('https://cloud.agilefant.com:443/wildergonzo/api/v1/stories/' + storyId)
+	.expectStatus(200)
+  .expectJSON('0', {
+		'type': 'story',
+		'id': 652018
+	})
+  //.inspectJSON()
+.toss();
+
+var commentsMatch = true;
+frisby.create('agilefant should return all comment from a story')
+	.get('https://cloud.agilefant.com:443/wildergonzo/api/v1/stories/' + storyId + '/comments')
+	.expectStatus(200)
+	.afterJSON(function(comments){
+			for (var i=0; i<comments.length; i++){
+					if (expectedComments.indexOf(comments[i].id) === -1) {
+							commentsMatch = false;
+					}
+					expect(comments[i].type).toBe('comment');
+			}
+			expect(commentsMatch).toBeTruthy();
+	})
+.toss();
