8d1293ab264efd34aff0bdd9bfb9830ae9d360ce
wilder_laptop <wilder.gonzales@gmail.com>
2015-08-06 23:10:03 -0400

    superagent: updated modules structure

---
+++ training/back-end/spec/api/domain-objects/auth-manager.js
@@ -0,0 +1,19 @@
+var request = require('superagent');
+var authorized = false,
+	authorization = '';
+var authManager = {
+	authorize: function (username, password){
+		authorization = 'Basic d2lsZGVyZ29uem8udGVzdEBnbWFpbC5jb206Q29udHJvbDEyMw==';
+		authorized = true;
+	},
+	isAuthorized: function (){
+		return authorized;
+	},
+	getAuthorization: function (){
+		return authorization;
+	},
+	revokeAuthorization: function (){
+		return false;
+	}
+};
+module.exports = authManager;
--- training/back-end/spec/api/superagent/backlog-spec.js
+++ training/back-end/spec/api/superagent/backlog-spec.js
@@ -1,43 +1,36 @@
 var request = require('superagent');
-var autorizationToken = 'Basic d2lsZGVyZ29uem8udGVzdEBnbWFpbC5jb206Q29udHJvbDEyMw==';
+var backlog = require('../domain-objects/backlogs.js');
 var backlogId = {stories: 145526, tasks: 145761};
 var expectedStories = [652018, 650888, 650887];
 
-describe('Todo.ly SuperAgent Tests', function(){
+describe('Agilefant SuperAgent Tests', function(){
 	//beforeEach(function (){});
 	//aferEach(function (){});
-    
+
     var storiesMatch = true;
-    it('agilefant should update the name of an existent backlog', function (done){
-		request
-			.get('https://cloud.agilefant.com:443/wildergonzo/api/v1/backlogs/' + backlogId.stories + '/stories')
-			.set('Authorization', autorizationToken)
-			.end(function (error, response){
-                var stories = response.body;
-				for (var i=0; i<stories.length; i++){
-					if (expectedStories.indexOf(stories[i].id) === -1) {
-							storiesMatch = false;
-					}
-					expect(stories[i].type).toBe('story');
-                }
-                expect(storiesMatch).toBeTruthy();
-				done();
-			});
-	});
-	
-	/*var backlogData = {
-       "type": "product",
-       "name": "backlogToBeUpdated"
-    };
-	it('agilefant should update the name of an existent backlog', function (done){
-		request
-			.post('https://cloud.agilefant.com:443/wildergonzo/api/v1/backlogs')
-			.set('Authorization', autorizationToken)
-			.send(backlogData)
-			.end(function (error, response){
-                console.log(response.body)
-				expect(response.body.type).toBe('product');
-				done();
-			});
-	});*/
+
+    it('agilefant should retrieve all stories from a backlog given an id', function (done){
+        backlog.getAll(backlogId.stories, function (response) {
+          var stories = response.body;
+          for (var i=0; i<stories.length; i++){
+            if (expectedStories.indexOf(stories[i].id) === -1) {
+                storiesMatch = false;
+            }
+            expect(stories[i].type).toBe('story');
+          }
+          expect(storiesMatch).toBeTruthy();
+          done();
+        });
+	  });
+
+    it('agilefant should create a new backlog', function (done){
+      var newBacklog = {
+         "type": "product",
+         "name": "testCreateBacklog"
+      };
+      backlog.create(newBacklog, function (response){
+          expect(response.body[0].type).toBe('product');
+          done();
+      });
+	  });
 });
