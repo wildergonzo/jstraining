4976cfba495ee523cb4c39c930ec7a8cbce00213
wilder_laptop <wilder.gonzales@gmail.com>
2015-08-06 23:16:37 -0400

    applied lint

--- training/back-end/spec/api/domain-objects/auth-manager.js
+++ training/back-end/spec/api/domain-objects/auth-manager.js
@@ -1,19 +1,19 @@
 var request = require('superagent');
 var authorized = false,
-	authorization = '';
+    authorization = '';
 var authManager = {
-	authorize: function (username, password){
-		authorization = 'Basic d2lsZGVyZ29uem8udGVzdEBnbWFpbC5jb206Q29udHJvbDEyMw==';
-		authorized = true;
-	},
-	isAuthorized: function (){
-		return authorized;
-	},
-	getAuthorization: function (){
-		return authorization;
-	},
-	revokeAuthorization: function (){
-		return false;
-	}
+    authorize: function (username, password) {
+        authorization = 'Basic d2lsZGVyZ29uem8udGVzdEBnbWFpbC5jb206Q29udHJvbDEyMw==';
+        authorized = true;
+    },
+    isAuthorized: function () {
+        return authorized;
+    },
+    getAuthorization: function () {
+        return authorization;
+    },
+    revokeAuthorization: function () {
+        return false;
+    }
 };
 module.exports = authManager;
